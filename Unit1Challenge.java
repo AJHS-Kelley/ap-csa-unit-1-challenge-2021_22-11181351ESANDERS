59a19f7149aecef2d9b807f32e49264ef1990f8e
11181351ESANDERS <90394029+11181351ESANDERS@users.noreply.github.com>
2021-10-20 09:34:29 -0400

    Update Unit1Challenge.java
    version v0.1
    added prints for entering user name, string for username, print for what the username is

--- Unit1Challenge.java
+++ Unit1Challenge.java
@@ -1,5 +1,16 @@
-// <Student Name>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <Date>, <Time>, v0.0
+// <E'mare Sanders>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <Time>, v0.0
 
-public class ? {
-    
-}
+import java.util.Scanner;
+
+public class DamageDealt {
+public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+
+    System.out.println("please Enter your Username: ")
+
+    String userName = myScanner.nextLine();
+
+    System.out.Println("Your username is:" + Username);
+
+    int myInt = myScanner.nextInt(); // Scan integers.
+    double myDouble = myScanner.nextDouble(); // Scan doubles
